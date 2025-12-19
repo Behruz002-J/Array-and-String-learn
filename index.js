2190a20a0dc9812e47a9f2db3982b2d3588736d4
Behruz Jumanov <behruzjumanov55@gmail.com>
2025-12-19 13:15:13 +0500

    added new changes

---
+++ index.js
@@ -0,0 +1,42 @@
+//1----
+//1.1----
+// let num = [];
+// num.push(1, 2, 3, 4, 5);
+// let del = num.pop(4) * 2;
+
+// num.push(del)
+// console.log(num);
+
+//1.2----
+// let word = ["Salom", "Dunyo", "Javascript"];
+// let n = word.pop(2);
+// let arxiv = [];
+// arxiv.push(n);
+// console.log(arxiv);
+
+//1.3----
+// let massiv = [4, 10, 40, 32, 11];
+//  let a  = massiv.pop(0);
+//  let b = massiv.pop(1);
+//  let c  = massiv.pop(2);
+//  let d  = massiv.pop(3);
+//  let e  = massiv.pop(4);
+//  console.log(a + b + c + d + e);
+ 
+ //2----
+//2.1----
+// let navbat = ['Ali', 'Vali', 'Gani'];
+// navbat.shift();
+// console.log(navbat);
+// navbat.unshift('Eshmat oka');
+// console.log(navbat);
+
+//  navbat.unshift('Ali xaridni tugatdi uje');
+// console.log(navbat);
+
+//2.2----
+let age = [100, 223, 433, 431, 343];
+let once = age.shift();
+let l = age.push(once);
+console.log(l);
+
