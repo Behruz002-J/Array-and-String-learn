530ef06d050c00312d932f5f4c7b9c251d76a156
Behruz Jumanov <behruzjumanov55@gmail.com>
2025-12-21 00:36:24 +0500

    added new changes

--- string.js
+++ string.js
@@ -1,56 +1,56 @@
 //1----charAt(index)
-let index = "JavaScript";
+// let index = "JavaScript";
 
-console.log(index.charAt(4));
+// console.log(index.charAt(4));
 
 //2-concat()
-let arr = "Behruz";
-console.log(arr.concat(100));
+// let arr = "Behruz";
+// console.log(arr.concat(100));
 
 //3-replace()
-let num = "Hello, Who";
-let newstr = num.replace("world", "daily");
-console.log(newstr);
+// let num = "Hello, Who";
+// let newstr = num.replace("world", "daily");
+// console.log(newstr);
 
 //4-split();
-let str = "olma,Banan,Anjir";
-let fruits = str.split(",");
-console.log(fruits);
+// let str = "olma,Banan,Anjir";
+// let fruits = str.split(",");
+// console.log(fruits);
 
 //4-substr(start,length);
-let string = "Daniel Maccon";
-console.log(str.substr(5, 7));
+// let string = "Daniel Maccon";
+// console.log(str.substr(5, 7));
 
 //5-substring
-let di = "Javlon";
-console.log(di.substring(4, 4));
+// let di = "Javlon";
+// console.log(di.substring(4, 4));
 
 //6-slice()
-let nine = [12, 30, 133, 300];
-let part = nine.slice(3, 5);
-console.log(part);
+// let nine = [12, 30, 133, 300];
+// let part = nine.slice(3, 5);
+// console.log(part);
 
 //7-tolower
-let narsa = "JADUEDEBDB";
-console.log(narsa.toLowerCase());
+// let narsa = "JADUEDEBDB";
+// console.log(narsa.toLowerCase());
 //8-upper
-let buyum = "faqat";
-console.log(buyum.toUpperCase());
+// let buyum = "faqat";
+// console.log(buyum.toUpperCase());
 
 //9-trim)()
-matn = "Salom Dunyo!";
-matn = text.trim();
-console.log(natija); // "Salom Dunyo!"
+// matn = "Salom Dunyo!";
+// matn = text.trim();
+// console.log(natija); // "Salom Dunyo!"
 
 //include
 
-let text = "Salom dunyo, koinotga xush kelibsiz.";
-mine = text.includes("dunyo");
-console.log(mine); // rost
+// let text = "Salom dunyo, koinotga xush kelibsiz.";
+// mine = text.includes("dunyo");
+// console.log(mine); // rost
 
 //10
 // search
 
-let janob = "Kok koz bor";
-let bot = janob.search("kok");
-console.log(bot);
+// let janob = "Kok koz bor";
+// let bot = janob.search("kok");
+// console.log(bot);
