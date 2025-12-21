8f16eb3abde30ab2a8c294d1bab10c57732562e5
Behruz Jumanov <behruzjumanov55@gmail.com>
2025-12-21 16:45:39 +0500

    added new changes

--- function.js
+++ function.js
@@ -1,32 +1,106 @@
-function salomlashish(ism) {
-    console.log("Salom yaxshimisan", ism);
+// function salomlashish(ism) {
+//     console.log("Salom yaxshimisan", ism);
     
-}
-salomlashish('Ali');
+// }
+// salomlashish('Ali');
 
 //2----
-const hisoblash = function(oylik) {
-    let month =  oylik / 12;
-    if(oylik / 12) {
-        console.log("bir oylik maoshingiz= ", month);
+// const hisoblash = function(oylik) {
+//     let month =  oylik / 12;
+//     if(oylik / 12) {
+//         console.log("bir oylik maoshingiz= ", month);
         
-    }
-return month;    
+//     }
+// return month;    
 
 
-}
-hisoblash(900000);
+// }
+// hisoblash(900000);
 
 //3----
-function kopaytirish(x, y) {
-    let n = x * y;
-if(n === 0) {
-    console.log("ko'paytma = " + n);
+// function kopaytirish(x, y) {
+//     let n = x * y;
+// if(n === 0) {
+//     console.log("ko'paytma = " + n);
     
-}
-return n;
-}
-kopaytirish(4, 5);
+// }
+// return n;
+// }
+// kopaytirish(4, 5);
 
 //4----
- 
+//  const somga = (dollar) => {
+//    usd = dollar * 9000;
+//    console.log(usd);
+   
+//  }
+// somga(1250000) ;
+
+//5----
+// const dollarga = (som) => {
+//     d = som * 12500;
+//     console.log(d);
+
+// }
+// dollarga(1100000);
+
+//6----
+// const masofaHisobla = function(tezlik, vaqt) {
+//    return  S = tezlik * vaqt;
+  
+// }
+// console.log(masofaHisobla(60, 2));
+// //7----
+// function length(text) {
+    
+// }
+
+//8----
+// function fibonacci(n) {
+//     let a = 1, b = 1;
+//     for (let i  =2; i <= n; i++) {
+//         let m = a + b;
+//         a = b;
+//         b = m;
+//     }
+//     return n === 0 ?  0 : b;
+// }
+// console.log(fibonacci(10));
+
+//9----
+// const palindrommi = function(matn) {
+//     if(matn === matn.split('').reverse().join('')) {
+//         console.log("Palindorm");
+        
+//     }else {
+//         console.log("Palindorm emas");
+        
+//     }
+// }
+// palindrommi("radar");
+//10----
+// function yoshniHisobla(tugilganYil) {
+// return (2002 -tugilganYil);
+// };
+// console.log(yoshniHisobla(1990));
+
+//String-easy-------
+//1----
+// let number = [1, 2, 3, 4, 5];
+// number.reverse();
+// console.log(number);
+
+//2----
+// let salom = "hello";
+// console.log(salom.toUpperCase());
+
+//3----
+// let massiv = [1, 2, 3];
+// massiv.push(4);
+// console.log(massiv);
+
+//4----
+// let length = [7, 8, 9];
+// console.log(length.length);
+
+//5----
